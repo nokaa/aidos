f19a7a82bb168965b365e8e1b2c969a8f5ce70ff
nokaa <nokaa@cock.li>
2016-03-17 15:39:52 -0500

    Seperated bang and search

--- search.go
+++ search.go
@@ -23,7 +23,29 @@ func SearchHandler(w http.ResponseWriter, r *http.Request) {
 
 	search := r.FormValue("q")
 	if strings.HasPrefix(search, "!") {
+		site, search := split(search)
 	} else {
 		http.Redirect(w, r, "https://duckduckgo.com/?q="+search, 302)
 	}
+}
+
+func split(search string) (string, string) {
+	j := 1
+	var site string
+	var searchString string
+
+	for i := j; i < len(search); i++ {
+		if search[i] == ' ' {
+			j = i + 1
+			break
+		} else {
+			site += string(search[i])
+		}
+	}
+
+	for i := j; i < len(search); i++ {
+		searchString += string(search[i])
+	}
+
+	return site, searchString
 }
