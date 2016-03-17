b4fccfac8f4f868fceb98010dfcc403a83254049
nokaa <nokaa@cock.li>
2016-03-17 15:13:03 -0500

    Handle non-POST requests

--- search.go
+++ search.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+	http.HandleFunc("/", SearchHandler)
+
+	fmt.Println("Listening at localhost:3000")
+	http.ListenAndServe(":3000", nil)
+}
+
+func SearchHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		fmt.Fprintf(w, `<html><body>This server is POST only. Please
+		<a href="https://git.nokaa.moe/nokaa/search">see the repository</a>
+		for more information.</body></html>`)
+		return
+	}
 }
