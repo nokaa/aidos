3d6775d1702970d32137f44e34544b02ac80f1a2
nokaa <nokaa@cock.li>
2016-03-18 12:17:57 -0500

    Switch to GET requests

--- search.go
+++ search.go
@@ -18,7 +18,7 @@ func main() {
 }
 
 func SearchHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
+	if r.Method != "GET" {
 		fmt.Fprintf(w, `<html><body>This server is POST only. Please
 		<a href="https://git.nokaa.moe/nokaa/search">see the repository</a>
 		for more information.</body></html>`)
